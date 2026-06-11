4a11799543f7e7e797e0540d7e0ca244030c9c9a
zalsay <51143152+zalsay@users.noreply.github.com>
2026-06-11 14:37:08 +0800

    Add shared JSON response helper

---
+++ internal/handlers/response.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func writeJSON(w http.ResponseWriter, status int, v interface{}) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(v)
+}
